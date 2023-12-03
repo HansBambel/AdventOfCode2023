7a040b21ae668e9b3bace690d801c0427dc058f9
Kevin Trebing <kevin.trebing@plan-d.com>
2023-12-03 09:56:11 +0100

    Day 3 refactor

--- src/adventofcode_2023/day_03/day_03.py
+++ src/adventofcode_2023/day_03/day_03.py
@@ -1,24 +1,22 @@
 from pathlib import Path
 
 
-def _in_boundary(coord: tuple[int, int], grid: list[str]) -> bool:
-    x, y = coord
+def _in_boundary(grid: list[str], x: int, y: int) -> bool:
     if x < 0 or x >= len(grid[0]) or y < 0 or y >= len(grid):
         return False
     return True
 
 
-def _get_number_with_coords(grid: list[str], coords: tuple[int, int]) -> tuple[int, set[tuple[int, int]]]:
+def _get_number_with_coords(grid: list[str], x: int, y: int) -> tuple[int, set[tuple[int, int]]]:
     # go to the left most index from the coords that is still a digit
-    y = coords[1]
-    x_left, x_right = coords[0], coords[0]
+    x_left, x_right = x, x
     number_coords = set()
-    while _in_boundary((x_left, y), grid) and grid[y][x_left].isdigit():
+    while _in_boundary(grid, x_left, y) and grid[y][x_left].isdigit():
         x_left -= 1
-        number_coords.add((x_left, coords[1]))
-    while _in_boundary((x_right, y), grid) and grid[y][x_right].isdigit():
+        number_coords.add((x_left, y))
+    while _in_boundary(grid, x_right, y) and grid[y][x_right].isdigit():
         x_right += 1
-        number_coords.add((x_right, coords[1]))
+        number_coords.add((x_right, y))
     found_number = grid[y][x_left + 1 : x_right]
     return int(found_number), number_coords
 
@@ -36,38 +34,44 @@ def _get_symbol_data(grid: list[str]) -> dict[tuple[int, int], str]:
     return symbols
 
 
+def get_surrounding_part_numbers(input_data, x, y):
+    symbol_part_numbers = []
+    # check neighbors for numbers
+    neighbors_to_check = {
+        (x - 1, y - 1),
+        (x, y - 1),
+        (x + 1, y - 1),
+        (x - 1, y),
+        (x + 1, y),
+        (x - 1, y + 1),
+        (x, y + 1),
+        (x + 1, y + 1),
+    }
+    checked_surroundings = set()
+    for check_x, check_y in neighbors_to_check:
+        if not _in_boundary(input_data, check_x, check_y) or (check_x, check_y) in checked_surroundings:
+            continue
+        checked_surroundings.add((check_x, check_y))
+        if input_data[check_y][check_x].isdigit():
+            part_number, number_coords = _get_number_with_coords(input_data, check_x, check_y)
+            symbol_part_numbers.append(part_number)
+            # add those from number_coords to checked_surroundings that
+            # also occur in neighbors_to_check using a set operation
+            neighbors_checked = neighbors_to_check.intersection(number_coords)
+            checked_surroundings = checked_surroundings.union(neighbors_checked)
+    return symbol_part_numbers
+
+
 def part_1(input_file: str):
     data_file = Path(__file__).with_name(input_file).read_text()
     input_data = data_file.split("\n")
     symbols = _get_symbol_data(input_data)
 
-    part_numbers = []
+    total = 0
     for (x, y), _ in symbols.items():
-        # check neighbors for numbers
-        neighbors_to_check = {
-            (x - 1, y - 1),
-            (x, y - 1),
-            (x + 1, y - 1),
-            (x - 1, y),
-            (x + 1, y),
-            (x - 1, y + 1),
-            (x, y + 1),
-            (x + 1, y + 1),
-        }
-        checked_surroundings = set()
-        for check_x, check_y in neighbors_to_check:
-            if not _in_boundary((check_x, check_y), input_data) or (check_x, check_y) in checked_surroundings:
-                continue
-            checked_surroundings.add((check_x, check_y))
-            if input_data[check_y][check_x].isdigit():
-                part_number, number_coords = _get_number_with_coords(input_data, (check_x, check_y))
-                part_numbers.append(part_number)
-                # add those from number_coords to checked_surroundings that
-                # also occur in neighbors_to_check using a set operation
-                neighbors_checked = neighbors_to_check.intersection(number_coords)
-                checked_surroundings = checked_surroundings.union(neighbors_checked)
-
-    return sum(part_numbers)
+        symbol_part_numbers = get_surrounding_part_numbers(input_data, x, y)
+        total += sum(symbol_part_numbers)
+    return total
 
 
 def part_2(input_file: str):
@@ -78,33 +82,10 @@ def part_2(input_file: str):
     for (x, y), symbol in symbols.items():
         if symbol != "*":
             continue
+        symbol_part_numbers = get_surrounding_part_numbers(input_data, x, y)
 
-        part_numbers = []
-        # check neighbors for numbers
-        neighbors_to_check = {
-            (x - 1, y - 1),
-            (x, y - 1),
-            (x + 1, y - 1),
-            (x - 1, y),
-            (x + 1, y),
-            (x - 1, y + 1),
-            (x, y + 1),
-            (x + 1, y + 1),
-        }
-        checked_surroundings = set()
-        for check_x, check_y in neighbors_to_check:
-            if not _in_boundary((check_x, check_y), input_data) or (check_x, check_y) in checked_surroundings:
-                continue
-            checked_surroundings.add((check_x, check_y))
-            if input_data[check_y][check_x].isdigit():
-                part_number, number_coords = _get_number_with_coords(input_data, (check_x, check_y))
-                part_numbers.append(part_number)
-                # add those from number_coords to checked_surroundings that
-                # also occur in neighbors_to_check using a set operation
-                neighbors_checked = neighbors_to_check.intersection(number_coords)
-                checked_surroundings = checked_surroundings.union(neighbors_checked)
-        if len(part_numbers) == 2:
-            total += part_numbers[0] * part_numbers[1]
+        if len(symbol_part_numbers) == 2:
+            total += symbol_part_numbers[0] * symbol_part_numbers[1]
     return total
 
 
