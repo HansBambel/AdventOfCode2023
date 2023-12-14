e712fc4147c558b673c6e6cbcd5020d649416e15
Kevin Trebing <kevin.trebing@plan-d.com>
2023-12-14 09:10:45 +0100

    Day 14 part 1

---
+++ src/adventofcode_2023/day_14/main.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+
+
+def count_load(grid) -> int:
+    load = 0
+    for y, line in enumerate(grid):
+        for x, symbol in enumerate(line):
+            if symbol == "O":
+                load += len(grid) - y
+
+    return load
+
+
+def tilt_grid(grid) -> list[str]:
+    """Tilt grid north. All O will move as far north as they can or hit a #."""
+    tilted_grid = ["" for _ in range(len(grid))]
+    for x in range(len(grid[0])):
+        count_round = 0
+        block = -1
+        for y in range(len(grid)):
+            if grid[y][x] == "#":
+                # move the current round_rocks to the block
+                for i in range(count_round):
+                    tilted_grid[block + i + 1] = tilted_grid[block + i + 1] + "O"
+                block = y
+                count_round = 0
+            if grid[y][x] == "O":
+                count_round += 1
+            y += 1
+        for i in range(count_round):
+            tilted_grid[block + i + 1] = tilted_grid[block + i + 1] + "O"
+
+    return tilted_grid
+
+
+def part_1(input_file: str):
+    data_file = Path(__file__).with_name(input_file).read_text()
+    grid = data_file.split("\n")
+
+    tilted_grid = tilt_grid(grid)
+
+    return count_load(tilted_grid)
+
+
+def part_2(input_file: str):
+    data_file = Path(__file__).with_name(input_file).read_text()
+    input_data = data_file.split("\n")
+
+
+if __name__ == "__main__":
+    print("#" * 10 + " Part 1 " + "#" * 10)
+    result_ex = part_1("input_ex.txt")
+    print(result_ex)
+    assert result_ex == 136
+
+    result = part_1("input.txt")
+    print(result)
+
+    # #### Part 2 ####
+    print("#" * 10 + " Part 2 " + "#" * 10)
+    result = part_2("input_ex.txt")
+    print(result)
+    assert result == 64
+
+    result = part_2("input.txt")
+    print(result)
