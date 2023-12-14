8918c9842b6bc588a4d8f6b56e6078706dd500c6
Kevin Trebing <kevin.trebing@plan-d.com>
2023-12-14 10:09:00 +0100

    Day 14 part 2 unfinished and bad

--- src/adventofcode_2023/day_14/main.py
+++ src/adventofcode_2023/day_14/main.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
+from tqdm import tqdm
+
 
 def count_load(grid) -> int:
     load = 0
     for y, line in enumerate(grid):
-        for x, symbol in enumerate(line):
+        for symbol in line:
             if symbol == "O":
                 load += len(grid) - y
 
@@ -22,29 +24,50 @@ def tilt_grid(grid) -> list[str]:
                 # move the current round_rocks to the block
                 for i in range(count_round):
                     tilted_grid[block + i + 1] = tilted_grid[block + i + 1] + "O"
+                # fill up with . up until current block
+                for i in range(block + 1 + count_round, y):
+                    tilted_grid[i] = tilted_grid[i] + "."
+                # add the block #
+                tilted_grid[y] = tilted_grid[y] + "#"
                 block = y
                 count_round = 0
             if grid[y][x] == "O":
                 count_round += 1
-            y += 1
         for i in range(count_round):
             tilted_grid[block + i + 1] = tilted_grid[block + i + 1] + "O"
+        # fill up with . up until current block
+        for i in range(block + 1 + count_round, len(grid)):
+            tilted_grid[i] = tilted_grid[i] + "."
 
     return tilted_grid
 
 
-def part_1(input_file: str):
+def part_1(input_file: str, tilted_count=1):
     data_file = Path(__file__).with_name(input_file).read_text()
     grid = data_file.split("\n")
 
     tilted_grid = tilt_grid(grid)
+    for _ in range(tilted_count - 1):
+        # transpose the grid
+        transposed = list(map(list, zip(*tilted_grid)))
+        tilted_grid = ["".join(x) for x in transposed]
 
     return count_load(tilted_grid)
 
 
-def part_2(input_file: str):
+def part_2(input_file: str, tilted_count=1):
     data_file = Path(__file__).with_name(input_file).read_text()
-    input_data = data_file.split("\n")
+    grid = data_file.split("\n")
+
+    tilted_grid = tilt_grid(grid)
+    for _ in tqdm(range(tilted_count - 1)):
+        # transpose the grid
+        # TODO gets transposed in the wrong direction
+        transposed = list(map(list, zip(*tilted_grid)))
+        tilted_grid = ["".join(x) for x in transposed]
+        tilted_grid = tilt_grid(tilted_grid)
+
+    return count_load(tilted_grid)
 
 
 if __name__ == "__main__":
@@ -58,9 +81,13 @@ if __name__ == "__main__":
 
     # #### Part 2 ####
     print("#" * 10 + " Part 2 " + "#" * 10)
-    result = part_2("input_ex.txt")
+
+    result = part_2("input_ex.txt", tilted_count=4)
+    print(result)
+
+    result = part_2("input_ex.txt", tilted_count=1000000000)
     print(result)
     assert result == 64
 
-    result = part_2("input.txt")
+    result = part_2("input.txt", tilted_count=1000000000)
     print(result)
