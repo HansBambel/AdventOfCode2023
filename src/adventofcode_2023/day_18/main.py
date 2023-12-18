7af02a1a0a8305d97ba370bb96500216493f623d
Kevin Trebing <kevin.trebing@plan-d.com>
2023-12-18 18:08:27 +0100

    day 18 part 2 lol

--- src/adventofcode_2023/day_18/main.py
+++ src/adventofcode_2023/day_18/main.py
@@ -1,6 +1,22 @@
 from pathlib import Path
 
-from matplotlib import path
+import numpy as np
+
+
+def polygon_area(x, y):
+    """Shoelace formula: https://stackoverflow.com/a/49129646/7132596"""
+    correction = x[-1] * y[0] - y[-1] * x[0]
+    main_area = np.dot(x[:-1], y[1:]) - np.dot(y[:-1], x[1:])
+    return 0.5 * np.abs(main_area + correction)
+
+
+def get_area_with_boundary(points: list[tuple[int, int]]) -> int:
+    path_len = sum(
+        [abs(points[i][0] - points[i + 1][0]) + abs(points[i][1] - points[i + 1][1]) for i in range(len(points) - 1)]
+    )
+    dig_arr = np.array(points)
+    inner_area = polygon_area(dig_arr[:, 0], dig_arr[:, 1])
+    return int(inner_area) + 1 + path_len // 2
 
 
 def part_1(input_file: str):
@@ -8,41 +24,37 @@ def part_1(input_file: str):
     input_data = data_file.split("\n")
 
     dig_path = [(0, 0)]
+    direction_map = {"U": (-1, 0), "D": (1, 0), "L": (0, -1), "R": (0, 1)}
     for line in input_data:
-        direction, distance, hexcode = line.split(" ")
+        direction, distance, _ = line.split(" ")
         distance = int(distance)
-        match direction:
-            case "U":
-                add_coord = (-1, 0)
-            case "D":
-                add_coord = (1, 0)
-            case "L":
-                add_coord = (0, -1)
-            case "R":
-                add_coord = (0, 1)
-            case _:
-                add_coord = (0, 0)
+        add_coord = direction_map[direction]
+        last_loc = dig_path[-1]
+        dig_path.append((last_loc[0] + distance * add_coord[0], last_loc[1] + distance * add_coord[1]))
 
-        dig_path.append((dig_path[-1][0] + distance * add_coord[0], dig_path[-1][1] + distance * add_coord[1]))
-
-    min_y, max_y = min([coord[0] for coord in dig_path]), max([coord[0] for coord in dig_path])
-    min_x, max_x = min([coord[1] for coord in dig_path]), max([coord[1] for coord in dig_path])
-    whole_grid = [(x, y) for y in range(min_y, max_y + 1) for x in range(min_x, max_x + 1)]
-    dig_path_grid = path.Path(dig_path)
-    inner = dig_path_grid.contains_points(whole_grid)
-    path_len = sum(
-        [
-            abs(dig_path[i][0] - dig_path[i + 1][0]) + abs(dig_path[i][1] - dig_path[i + 1][1])
-            for i in range(len(dig_path) - 1)
-        ]
-    )
-
-    return sum(inner) + path_len
+    return get_area_with_boundary(dig_path)
 
 
 def part_2(input_file: str):
     data_file = Path(__file__).with_name(input_file).read_text()
     input_data = data_file.split("\n")
+
+    dig_path = [(0, 0)]
+    direction_map = {"U": (-1, 0), "D": (1, 0), "L": (0, -1), "R": (0, 1)}
+    dir_remap = {"0": "R", "1": "D", "2": "L", "3": "U"}
+    for line in input_data:
+        # convert the hexcode
+        _, _, hexcode = line.split(" ")
+        hexcode = hexcode[1:-1]
+        distance = int(hexcode[1:-1], 16)
+        direction = dir_remap[hexcode[-1]]
+
+        distance = int(distance)
+        add_coord = direction_map[direction]
+        last_loc = dig_path[-1]
+        dig_path.append((last_loc[0] + distance * add_coord[0], last_loc[1] + distance * add_coord[1]))
+
+    return get_area_with_boundary(dig_path)
 
 
 if __name__ == "__main__":
@@ -59,7 +71,7 @@ if __name__ == "__main__":
     print("#" * 10 + " Part 2 " + "#" * 10)
     result = part_2("input_ex.txt")
     print(result)
-    assert result == 1337
+    assert result == 952408144115
 
     result = part_2("input.txt")
     print(result)
